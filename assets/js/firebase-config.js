7dcd8d7c98c6d1a126d6a95accee2dba136df783
Sam-oTerry <148712384+Sam-oTerry@users.noreply.github.com>
2025-07-10 15:26:49 +0300

    Enhance Firebase integration by initializing the app only once and adding Firestore and Auth SDKs. This update improves performance and ensures proper configuration management for Firebase services in the application.

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -17,12 +17,24 @@ function initializeFirebase() {
     return;
   }
   
-  if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
+  // Firebase config and initialization
+  if (!window.firebaseAppInitialized) {
+    const firebaseConfig = {
+      apiKey: "AIzaSyDH1sMk2NwceMAEfvH07azxaoPXpOI1Sek",
+      authDomain: "starlet-properties-41509.firebaseapp.com",
+      projectId: "starlet-properties-41509",
+      storageBucket: "starlet-properties-41509.appspot.com",
+      messagingSenderId: "393372988481",
+      appId: "1:393372988481:web:c92584d7408296457b02c0",
+      measurementId: "G-F02K9SP07C"
+    };
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+    }
+    window.firebaseAuth = firebase.auth();
+    window.firebaseDB = firebase.firestore();
+    window.firebaseAppInitialized = true;
   }
-  
-  window.firebaseDB = firebase.firestore();
-  window.firebaseAuth = firebase.auth();
   
   if (firebase.storage) {
     window.firebaseStorage = firebase.storage();
